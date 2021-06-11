7c6d5b6fc55f8ad5cb79d57c9e140b52e87109f3
Jaume Barbereb¡na <ubiest@hey.com>
2021-06-11 19:42:13 +0200

    changed the image passed

--- frontend/main.py
+++ frontend/main.py
@@ -22,7 +22,7 @@ if class_btn:
         with st.spinner('Model working....'):
             #### TODO passar imatge mitjançant request
             #files = {"file": file_uploaded.getvalue()}
-            files = {"file": image}
+            files = {"file": file_uploaded}
             res = requests.post(f"http://backend:8080/classify", files=files)
 
             #predictions, pred_dict = predict(image) ### Old style direct call
